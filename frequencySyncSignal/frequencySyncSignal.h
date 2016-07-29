ca93a339249ec51975ef35eb56a4e4ab0e683b8f
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2016-07-29 16:43:29 -0400

    first implementation

--- frequencySyncSignal/frequencySyncSignal.h
+++ frequencySyncSignal/frequencySyncSignal.h
@@ -6,6 +6,10 @@
 #include <vector>
 using namespace std;
 
+class oneRFOutput {
+
+};
+
 class FrequencySyncSignal {
 
 	// constructor from string.
@@ -13,11 +17,19 @@ class FrequencySyncSignal {
 	FrequencySyncSignal(string setup);
 
 private:
-	double timeWindow;
-	double startTime;
-	double radioFrequency;
-	double radioInterval;
-	double signalsDistance;
+	double timeWindow;     // total timewindow of one event
+	double startTime;      // event start time
+	double radioFrequency; // radiofrequency - in GHz
+	double radioInterval;  // radiointerval - in ns. It's 1/radioFrequency
+
+	double nRadiofrequency;  // number of multiple signals, defined at construction
+	double signalsDistance;  // distance between RF signals
+
+	// output is a 2D vector:
+	// The number of Radiofrequency banks is nRadiofrequency
+	// The number of Radiofrequency signals within one bank is defined by
+	// filling the timewindow based on the radioFrequency
+	vector<vector <double > > output;
 
 };
 
