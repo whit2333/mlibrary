5e5ab748c114fc760f82cbead96f49797e1fdf6b
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2016-09-21 16:45:39 -0400

    added qt headers

--- qtButtonsWidget/example.cc
+++ qtButtonsWidget/example.cc
@@ -19,9 +19,6 @@ int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
 
-
-	cout << argv[0] << endl;
-
 	// the exampleResources.rcc is obtained with:
 	// rcc -binary exampleResources.qrc -o exampleResources.rcc
 	// it is needed in the executable dir in case it is compiled by xcode
