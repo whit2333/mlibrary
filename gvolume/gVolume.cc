07bcd36f377a13ef06ec45067613d61307d9beaa
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-05-25 16:21:42 -0400

    implementing gvolume interface with gtable

--- gvolume/gVolume.cc
+++ gvolume/gVolume.cc
@@ -1,13 +1,55 @@
+// gVolume
 #include "gVolume.h"
+
+// the double brackets is used because
+// initializer lists ambiguity:
+//
+// std::vector<int> i{10, 0};    // 10 elements of value zero
+// std::vector<int> j{{10, 0}};  // 2 elements of value 10 and 0 respectively
+
+array<string, 20> gTableValidKeys = { {
+	"name",
+	"mother",
+	"description",
+	"factory",
+	"type",
+	"dimensions",
+	"visible",
+	"style",
+	"color",
+	"material",
+	"magfield",
+	"pos"       ,
+	"rot",
+	"sensitivity",
+	"copyOf",
+	"replicaOf",
+	"ncopy",
+	"solidsOpr",
+	"mirrorType",
+	"mirrorMat" } } ;
+
+array<string, 5> gVolumeNecessaryKeys = { {
+	"name",
+	"factory",
+	"type",
+	"dimensions",
+	"material",
+} } ;
+
+
+// c++
+#include <iostream>
 
 // mlibrary
 #include "gstring.h"
 using namespace gstring;
 
+
 // checks for key validity
 bool GTable::isValidKey(string thisKey)
 {
-	if(find(validKeys.begin(), validKeys.end(), thisKey) != validKeys.end()) {
+	if(find(gTableValidKeys.begin(), gTableValidKeys.end(), thisKey) != gTableValidKeys.end()) {
 		return true;
 	}
 
@@ -23,3 +65,87 @@ bool GTable::addData(string thisKey, string data)
 	}
 	return false;
 }
+
+string GTable::getData(string thisKey)
+{
+	if(isValidKey(thisKey)) {
+		// making sure the key exist
+		if(gtable.find(thisKey) != gtable.end()) {
+			return gtable[thisKey];
+		}
+	}
+	return "notDefined";
+}
+
+GVolume::GVolume(GTable gt)
+{
+	// initialize variables that could be omitted
+	initGVolumeMembers();
+
+	// making sure the necessary variables are there
+	for (auto &s : gVolumeNecessaryKeys) {
+		if(gt.getData(s) == "notDefined") {
+			cout << " !!! Fatal Error: key " << s << " not defined. Exiting. " << endl;
+			exit(0);
+		}
+	}
+
+	// now initializing members
+	// making sure the necessary variables are there
+	for (auto &s : gTableValidKeys) {
+		string tableValue = gt.getData(s);
+		if(tableValue != "notDefined") {
+				 if(s == "name")               name = tableValue;
+			else if(s == "mother")           mother = tableValue;
+			else if(s == "description") description = tableValue;
+			else if(s == "factory")         factory = tableValue;
+			else if(s == "type")               type = tableValue;
+			else if(s == "dimensions")   dimensions = tableValue;
+			else if(s == "visible")        visible  = (tableValue == "1") ? 1 : 0;
+			else if(s == "style")             style = stoi(tableValue);
+			else if(s == "color")             color = tableValue;
+			else if(s == "material")       material = tableValue;
+			else if(s == "magfield")       magfield = tableValue;
+			else if(s == "pos")                 pos = tableValue;
+			else if(s == "rot")                 rot = tableValue;
+			else if(s == "sensitivity") sensitivity = tableValue;
+			else if(s == "copyOf")          copyOf  = tableValue;
+			else if(s == "replicaOf")     replicaOf = tableValue;
+			else if(s == "ncopy")             ncopy = stoi(tableValue);
+			else if(s == "solidsOpr")     solidsOpr = tableValue;
+			else if(s == "mirrorType")   mirrorType = tableValue;
+			else if(s == "mirrorMat")     mirrorMat = tableValue;
+		}
+	}
+
+
+}
+
+
+// these member variables may be left unitialized
+void GVolume::initGVolumeMembers()
+{
+	mother      = "root";
+	description = "notGiven";
+	visible     = true;
+	style       = 1;
+	color       = "556699";
+	magfield    = "inherit";
+	pos         = "no";
+	rot         = "no";
+	sensitivity = "no";
+	copyOf      = "no";
+	replicaOf   = "no";
+	ncopy       = 0;
+	solidsOpr   = "no";
+	mirrorType  = "no";
+	mirrorMat   = "no";
+}
+
+
+
+
+
+
+
+
--- gvolume/gVolume.h
+++ gvolume/gVolume.h
@@ -1,5 +1,5 @@
 #ifndef  GVOLUME_H
-#define  GVOLUME_H
+#define  GVOLUME_H 1
 
 // c++
 #include <string>
@@ -11,45 +11,66 @@ using namespace std;
 //// options
 //#include "goptions.h"
 
+extern array<string, 20> gTableValidKeys;
+extern array<string, 20> GVolumeNecessaryKeys;
+
+
+// the class GTable is the interface to the detector
+class GTable {
+public:
+	GTable();
+
+private:
+	map<string, string> gtable;
+
+public:
+	bool addData(string key, string data);
+	string getData(string key);
+
+private:
+	bool isValidKey(string key);
+	
+};
+
 class GVolume
 {
 public:
-	GVolume();
+	GVolume(GTable gt);
 
 private:
-	string                 name;   ///< Name of the volume. Since this is the key of the STL map, it has to be unique.
-	string               mother;   ///< Mother Volume name.
-	string          description;   ///< Volume Description, for documentation.
-	string              factory;   ///< Provenience. It can be perl, pyton, cad or gdml
+	string        name;   ///< Name of the volume. Since this is the key of the STL map, it has to be unique.
+	string      mother;   ///< Mother Volume name.
+	string description;   ///< Volume Description, for documentation.
+	string     factory;   ///< Provenience. It can be perl, pyton, cad or gdml
 
 	// solid parameters
-	string                 type;   ///< solid type. This follows the GEANT4 definitions
-	vector<double>   dimensions;   ///< vector of dimensions. Size depends on solid type
+	string        type;   ///< solid type. This follows the GEANT4 definitions
+	string  dimensions;   ///< vector of dimensions. Size depends on solid type
 
 	// solid visualization style
-	bool                visible;   ///< visibility of the detector: 0=invisible 1=visible
-	int                   style;   ///< Visual style: 0=wireframe 1=solid
-	string                color;   ///< 6(7) digits colors in RRGGBB format. Last optional digit is transparency
+	bool        visible;   ///< visibility of the detector: 0=invisible 1=visible
+	int           style;   ///< Visual style: 0=wireframe 1=solid
+	string        color;   ///< 6(7) digits colors in RRGGBB format. Last optional digit is transparency
 
 	// logical attributes
-	string             material;   ///< Volume Material name.
-	string             magfield;   ///< Magnetic Field. The string "no" means that the field is inherited from the mother volume.
+	string     material;   ///< Volume Material name.
+	string     magfield;   ///< Magnetic Field. The string "no" means that the field is inherited from the mother volume.
 
 	// physical attributes
-	string                  pos;   ///< Position relative to the mother volume
-	string                  rot;   ///< Rotation Matrix, defined by rotations along x,y,z axis relative to the mother volume
+	string          pos;   ///< Position relative to the mother volume
+	string          rot;   ///< Rotation Matrix, defined by rotations along x,y,z axis relative to the mother volume
 
-	string          sensitivity;   ///< Assigns digitization type and collection ID
+	string  sensitivity;   ///< Assigns digitization type and collection ID
 
 	// special cases
-	string               copyOf;   ///< name of gvolume to copy from
-	string            replicaOf;   ///< name of gvolume to replica from
-	int                   ncopy;   ///< copy number, in case of copies and replicas
-	string       solidOperation;
+	string       copyOf;   ///< name of gvolume to copy from
+	string    replicaOf;   ///< name of gvolume to replica from
+	int           ncopy;   ///< copy number, in case of copies and replicas
+	string    solidsOpr;   ///< solids operation
 
 	// mirrors
 	string   mirrorType;
-	string   mirrorMaterial;
+	string    mirrorMat;
 
 	// defined in utilities
 	friend ostream &operator<<(ostream &stream, GVolume); ///< Logs infos on screen.
@@ -58,47 +79,9 @@ public:
 	// GVolume options
 //	static map<string, GOption> defineOptions();
 
+private:
+	void initGVolumeMembers();
 };
 
-
-// the class GTable is the interface to the detector
-class GTable {
-public:
-	GTable();
-	
-private:
-	array<string, 20> validKeys = {
-		"name",
-		"mother",
-		"description",
-		"factory",
-		"type",
-		"dimensions",
-		"visible",
-		"style",
-		"color",
-		"material",
-		"magfield",
-		"pos"       ,
-		"rot",
-		"sensitivity",
-		"copyOf",
-		"replicaOf",
-		"ncopy",
-		"solidOperation",
-		"mirrorType",
-		"mirrorMaterial" };
-
-	map<string, string> gtable;
-
-public:
-	bool addData(string key, string data);
-	string getData(string key);
-
-
-private:
-	bool isValidKey(string key);
-
-};
 
 #endif
