29e074c31c6e0ccdaa91ab1f3f95262c68ed99bc
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-06-05 16:41:51 -0400

    added g4volume

---
+++ g4volume/example.cc
@@ -0,0 +1,17 @@
+/// \file example.cc
+
+// example on how to use the g4volume library
+
+
+// c++
+#include <iostream>
+using namespace std;
+
+
+//! example of main declaring GOptions
+int main(int argc, char* argv[])
+{
+
+	return 1;
+}
+
---
+++ g4volume/g4volume.h
@@ -0,0 +1,4 @@
+#ifndef  G4VOLUME_H
+#define  G4VOLUME_H
+
+#endif
