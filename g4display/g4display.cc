a1b416400fee87397f95a17b5160077d2290c384
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-07-10 14:26:44 -0400

    todo log, rename lab

--- g4display/g4display.cc
+++ g4display/g4display.cc
@@ -24,6 +24,9 @@ vector<string> availableG4Viewers = {
  */
 G4Display::G4Display(GOptions* gopts)
 {
+	// PRAGMA: add resolution option, hits, here?
+	// PRAGMA: if run/beamOn is given (e.g. command line optios) then the label needs to be updated!
+
 	vector<string> viewOptions = gopts->getStringVectorValue("g4view");
 
 	string gViewOptions = "/vis/open " + getViewerFromOption(viewOptions) + " " + getGeoFromOption(viewOptions) + getPosFromOption(viewOptions);
@@ -31,9 +34,8 @@ G4Display::G4Display(GOptions* gopts)
 	G4UImanager* UIM = G4UImanager::GetUIpointer();
 
 	if(UIM) {
-		UIM->ApplyCommand("/vis/scene/create GHall");
-
-
+		UIM->ApplyCommand("/vis/scene/create gemcLab");
+		
 		UIM->ApplyCommand(gViewOptions);
 
 		UIM->ApplyCommand("/vis/scene/add/axes 0 0 0 100 cm");
