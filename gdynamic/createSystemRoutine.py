76c4b55739850e9dad40b19d0fd2267df9cb7185
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-08-16 08:31:39 -0400

    starting python to write out routine source

--- gdynamic/createSystemRoutine.py
+++ gdynamic/createSystemRoutine.py
@@ -1,14 +1,62 @@
-#!/usr/bin/python -B            # -B prevents writing compiled bytecode files. Otherwise python writes .pyc compiled versions of each module
+#!/usr/bin/python -B   # -B prevents writing .pyc compiled versions of each module
 
-import sys
+import sys, getopt
 
-print('Arguments:', len(sys.argv))
-print('List:', str(sys.argv))
+def printHelp():
+	print ''
+	print 'test.py -s <systemName> routineNames'
+	print 'Possible routine names:'
 
 
-filename = sys.argv[1]
-print('Filename:', filename)
+def parseSystem(argv):
+	try:
+		opts, args = getopt.getopt(argv,"hs:")
+	except getopt.GetoptError:
+		printHelp()
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			printHelp()
+			sys.exit()
+		elif opt == "-s":
+			sysName = arg
+       		return sysName
+
+def parseRoutines(argv):
+	try:
+		opts, args = getopt.getopt(argv,"hr:")
+	except getopt.GetoptError:
+		printHelp()
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			printHelp()
+			sys.exit()
+		elif opt == "-r":
+			rNames = arg
+       		return rNames
+
+
+def writeHeader(systemName):
+	sum = x + y
+	return sum
+
+
+
+
+# print('Arguments:', len(sys.argv))
+# print('List:', str(sys.argv))
+
+
+systemName = parseSystem(sys.argv[1:])
+routines   = parseRoutines(sys.argv[1:])
+
+print 'systemName:', systemName, ' routines: ', routines
 
 text_file = open('output.txt', 'w')
 text_file.write('my string i want to put in  a file')
 text_file.close()
+
+
+
+
