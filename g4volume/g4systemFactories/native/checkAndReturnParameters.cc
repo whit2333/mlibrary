88b1cadccc76784cd1c57d1954fd00e1df2fa77d
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-10-05 16:27:58 -0400

    Detector is global, so using cerr is ok

--- g4volume/g4systemFactories/native/checkAndReturnParameters.cc
+++ g4volume/g4systemFactories/native/checkAndReturnParameters.cc
@@ -43,10 +43,10 @@ vector<double> G4NativeSetupFactory::checkAndReturnParameters(GVolume *s)
 		// the first constructor must have a number of pars multiple of 3
 		// the second constructor must have a number of pars (-3) multiple of 2
 		if(actualNumberOfParameters%3 !=0 && (actualNumberOfParameters - 3)%2 != 0) {
-			G4cerr << " !!! Fatal Error: the number of parameters in the constructor of " << name << ", " << type
+			cerr << " !!! Fatal Error: the number of parameters in the constructor of " << name << ", " << type
 			<< " is " << actualNumberOfParameters <<  ":" << s->getParameters() << endl;
 
-			G4cerr << "      This does not match a " << type <<". Exiting." << endl << endl;
+			cerr << "      This does not match a " << type <<". Exiting." << endl << endl;
 			exit(99);
 		} else {
 			return parameters;
@@ -58,7 +58,7 @@ vector<double> G4NativeSetupFactory::checkAndReturnParameters(GVolume *s)
 		// the first constructor must have a number of pars (-4) multiple of 3
 		// the second constructor must have a number of pars (-4) multiple of 2
 		if((actualNumberOfParameters - 4)%3 !=0 && (actualNumberOfParameters - 3)%2 != 0) {
-			G4cerr << " !!! Fatal Error: the number of parameters in the constructor of " << name << ", " << type
+			cerr << " !!! Fatal Error: the number of parameters in the constructor of " << name << ", " << type
 			<< " is " << actualNumberOfParameters <<  ":" << s->getParameters() << endl;
 
 			cerr << "      This does not match a " << type <<". Exiting." << endl << endl;
