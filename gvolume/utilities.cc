de97f31f13e0a3e0f85c1bd81365f1aa51b602a2
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-11-08 10:08:01 -0500

    - added GOPTIONITEM symbol to standardize goption help list

--- gvolume/utilities.cc
+++ gvolume/utilities.cc
@@ -20,11 +20,11 @@ map<string, GOption> GSetup::defineOptions()
 
 	optionsMap["addSystem"] = GOption("Adds a system", NOTAPPLICABLE, "system", true);
 	optionsMap["addSystem"].addHelp("The mandatory arguments, separated by commas, are:\n");
-	optionsMap["addSystem"].addHelp(" - system name \n");
-	optionsMap["addSystem"].addHelp(" - system factory\n");
-	optionsMap["addSystem"].addHelp(" - Optional arguments are:\n");
-	optionsMap["addSystem"].addHelp(" - system variation (default: \"default\")\n");
-	optionsMap["addSystem"].addHelp(" - system run number (default: 1)\n");
+	optionsMap["addSystem"].addHelp(string(GOPTIONITEM) + "system name \n");
+	optionsMap["addSystem"].addHelp(string(GOPTIONITEM) + "system factory\n");
+	optionsMap["addSystem"].addHelp("Optional arguments are:\n");
+	optionsMap["addSystem"].addHelp(string(GOPTIONITEM) + "system variation (default: \"default\")\n");
+	optionsMap["addSystem"].addHelp(string(GOPTIONITEM) + "system run number (default: 1)\n");
 	optionsMap["addSystem"].addHelp(" Example: -addSystem=\"ctof, cad, default, 12\"\n");
 
 	optionsMap["setupDir"] = GOption("Path(s) to check for setup, separated by space", NOTAPPLICABLE, "system", true);
