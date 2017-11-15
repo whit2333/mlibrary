ed029e7de463720ddf788d874c00d4b7ba51a3c8
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-11-15 15:26:07 -0500

    - adding true infos variables

--- gdata/observables/gdetectorObservables.h
+++ gdata/observables/gdetectorObservables.h
@@ -12,14 +12,15 @@ class GDetectorObservables
 {
 public:
 	GDetectorObservables(string sdName) : detectorSDName(sdName) {
-		detectorObservables.clear();
 	}
 	
+	// when adding an observale we can clear its name and description and add them
+	// to the container class: saving memory, we dont need a description for every hit
 	void addDetectorObservables(GObservables *observables) {
 		
 		if(!(observables->getDescriptionAssigned())) {
-			varName = observables->getName();
-			varDesc = observables->getDesc();
+			observableName = observables->getName();
+			observableDesc = observables->getDesc();
 			observables->clearNamesAndDescriptions();
 		}
 		
@@ -28,25 +29,35 @@ public:
 	
 	// api to get data
 	string getName() {return detectorSDName;}
-	vector<string> getVarName() {return varName;}
-	vector<string> getVarDesc() {return varDesc;}
+	vector<string> getObservableName() {return observableName;}
+	vector<string> getObservableDesc() {return observableDesc;}
+	vector<string> getTrueInfoName()   {return trueInfoName;}
+	vector<string> getTrueInfoDesc()   {return trueInfoDesc;}
 
+	// observables api
 	vector<GObservables*> getDObservables()       {return detectorObservables;}
 	vector<GObservables*> getDObservablesPulses() {return detectorObservablePulses;}
+
+	// true infos api
+	vector<GObservables*> getTObservables()       {return detectorTrueInfos;}
+	vector<GObservables*> getTObservablesPulses() {return detectorObservablePulses;}
 
 private:
 	
 	// detector SD name, passed here to it can be published
 	string detectorSDName;
-	vector<string> varName;
-	vector<string> varDesc;
+	vector<string> observableName;
+	vector<string> observableDesc;
+	vector<string> trueInfoName;
+	vector<string> trueInfoDesc;
+
+	// observables: accumulating over hits
+	vector<GObservables*> detectorObservables;       // one instance of an observable / hit
+	vector<GObservables*> detectorObservablePulses;  // multiple instances of an observable / hit
 
 	// true infos: accumulating over hits
 	vector<GObservables*> detectorTrueInfos;       // one instance of true infos / hit
 	vector<GObservables*> detectorTrueInfosPulses; // multiple instances of true infos / hit
 	
-	// observables: accumulating over hits
-	vector<GObservables*> detectorObservables;       // one instance of an observable / hit
-	vector<GObservables*> detectorObservablePulses;  // multiple instances of an observable / hit
 };
 #endif
