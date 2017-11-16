4f6510d4c4e90b06e67357ab0155dfd814692075
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-11-16 15:44:02 -0500

    - units instead of description in the observables

--- gdynamic/trueObservable.cc
+++ gdynamic/trueObservable.cc
@@ -9,7 +9,7 @@ GObservables* GDynamic::trueInfoHit(GHit *ghit)
 	
 	gdata->addObservable(ghit->getTotalEnergyDeposited(),  // variable value
 						 "totEDep",                        // variable name
-						 "total energy deposited",         // variable description
+						 "MeV",         // variable description
 						 "double");                        // save to disk as type
 
 	
