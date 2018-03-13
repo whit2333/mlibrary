03e97ace06d1c26378223be7cbb63ea2f2ebf8f0
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-03-13 14:07:31 -0400

    - passing env to sconscript - better scons documentation - added dummy example.cc

---
+++ ghit/example.cc
@@ -0,0 +1,11 @@
+/// \file example.cc
+
+// example on how to use the g4displa library and the displayUI widget
+
+//! example of main declaring GOptions
+int main(int argc, char* argv[])
+{
+
+	return 1;
+}
+
