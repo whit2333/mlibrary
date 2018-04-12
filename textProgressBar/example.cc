f3ea5fe36a90914b2d391f2be9d6462e0b14e49e
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-04-12 16:35:11 -0400

    fixed some types on progress bar, added better comments

--- textProgressBar/example.cc
+++ textProgressBar/example.cc
@@ -14,13 +14,12 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 
-	int nevents = 100000000;
+	int nevents = 1000000000;
 
 	TextProgressBar bar(20, "  index progress: ", 1, nevents);
 
 	for(int i=1; i<nevents; i++) {
 		bar.setProgress(i);
-
 	}
 
 	return 1;
--- textProgressBar/textProgressBar.cc
+++ textProgressBar/textProgressBar.cc
@@ -15,24 +15,31 @@ void TextProgressBar::setProgress(int p)
 	// outside boundaries
 	if(p < min || p > max) return;
 
+	// progress is between 1 and 100
+	double progress = indexStep/singleStep;
 
+	// progress outside boundaries
+	if(progress > TEXTPROGRESSBARNSTEPS) return;
+
+	// adding the single step
 	indexStep += singleStep;
 
+	// printing header of the bar
 	cout  << title << startBarChar ;
 
-	// progress is between 1 and 100
-	int progress = indexStep/singleStep;
-
 	// pos is relative position within the bar
-	int pos = progress*barWidth/TEXTPROGRESSBARNSTEPS;
+	int pos = barWidth*(progress/TEXTPROGRESSBARNSTEPS);
 
+	// print bar, and advance char, empty space at the right places
 	for (int i = 0; i < barWidth; ++i) {
 		if      (i < pos)  cout << middleBarChar;
 		else if (i == pos) cout << advanceBarChar;
 		else cout << " ";
 	}
+	// print end bar and progress percentage
 	cout << endBarChar << " " << progress << " %\r" ;
 
+	// carriage return if needed
 	if(progress == TEXTPROGRESSBARNSTEPS) cout << endl;
 
 	cout.flush();
