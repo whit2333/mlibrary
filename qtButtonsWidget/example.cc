579273a1d1dae99853e7dd1c16770dcfa2338ebf
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-04-17 14:31:35 -0400

    added comments for qrc to rcc

--- qtButtonsWidget/example.cc
+++ qtButtonsWidget/example.cc
@@ -18,12 +18,6 @@ int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
 
-	// the exampleResources.rcc is obtained with:
-	// rcc -binary exampleResources.qrc -o exampleResources.rcc
-	// it is needed in the executable dir in case it is compiled by xcode
-	// scons on the other hand knows about it because the qt module
-	// compiles exampleResources.qrc directly
-
 	QFileInfo qrcFileInfoExecutable(argv[0]);
 	QString rccPath = qrcFileInfoExecutable.absolutePath() + "/" + "exampleResources.rcc";
 	QResource::registerResource(rccPath);
